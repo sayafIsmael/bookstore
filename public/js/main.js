094235b4c0e8db75cff2bbae3c50a45d49d466de
Sayaf Ismael <sayaf0007@gmail.com>
2019-07-15 13:39:39 +0600

    fixed all slider problem

--- public/js/main.js
+++ public/js/main.js
@@ -84,4 +84,246 @@ function initMap() {
 
 	
 
+	$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+  
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+
+  $('#carouselExample').carousel({ 
+                interval: 2000
+        });
+
+
+  $(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+      event.preventDefault();
+      var content = $('.modal-body');
+      content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);        
+        content.html($(this).html());
+        $(".modal-profile").modal({show:true});
+    });
+
+  });
+
+	(function() {
+		function loadJS(u){var r=document.getElementsByTagName("script")[ 0 ],s=document.createElement("script");s.src=u;r.parentNode.insertBefore(s,r);}
+	
+	if(!window.HTMLPictureElement){
+		document.createElement('picture');
+		//generating the config array
+		window.picturefillCFG = window.picturefillCFG || [];
+		picturefillCFG.push([ "algorithm", "saveData" ]);
+	
+		loadJS("https://cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.0-beta1/picturefill.min.js");
+	}loadJS
+	}());  
+	
+	(function() {
+		window.lazySizesConfig = window.lazySizesConfig || {};
+		window.lazySizesConfig.loadMode = 1;
+	
+		window.lazySizesConfig.expand = 222;
+		window.lazySizesConfig.expFactor = 1.6;
+	}());  
+	
+		/*
+			for this demo not needed because we use CSS intrinsic sizing,
+			but really useful if you deal with responsive images or lazyloaded images
+		*/
+		(function(){
+			var oldFlickityCreate = window.Flickity.prototype._create;
+	
+			window.Flickity.prototype._create = function(){
+				var that = this;
+				if(this.element.addEventListener){
+					this.element.addEventListener('load', function(){
+						that.onresize();
+					}, true);
+				}
+				this._create = oldFlickityCreate;
+				return oldFlickityCreate.apply(this, arguments);
+			};
+		})();
+
+		
+		
+(function($) {
+  "use strict";
+  $.fn.sliderResponsive = function(settings) {
+    
+    var set = $.extend( 
+      {
+        slidePause: 5000,
+        fadeSpeed: 800,
+        autoPlay: "on",
+        showArrows: "off", 
+        hideDots: "off", 
+        hoverZoom: "on",
+        titleBarTop: "off"
+      },
+      settings
+    ); 
+    
+    var $slider = $(this);
+    var size = $slider.find("> div").length; //number of slides
+    var position = 0; // current position of carousal
+    var sliderIntervalID; // used to clear autoplay
+      
+    // Add a Dot for each slide
+    $slider.append("<ul></ul>");
+    $slider.find("> div").each(function(){
+      $slider.find("> ul").append('<li></li>');
+    });
+      
+    // Put .show on the first Slide
+    $slider.find("div:first-of-type").addClass("show");
+      
+    // Put .showLi on the first dot
+    $slider.find("li:first-of-type").addClass("showli")
+
+     //fadeout all items except .show
+    $slider.find("> div").not(".show").fadeOut();
+    
+    // If Autoplay is set to 'on' than start it
+    if (set.autoPlay === "on") {
+        startSlider(); 
+    } 
+    
+    // If showarrows is set to 'on' then don't hide them
+    if (set.showArrows === "on") {
+        $slider.addClass('showArrows'); 
+    }
+    
+    // If hideDots is set to 'on' then hide them
+    if (set.hideDots === "on") {
+        $slider.addClass('hideDots'); 
+    }
+    
+    // If hoverZoom is set to 'off' then stop it
+    if (set.hoverZoom === "off") {
+        $slider.addClass('hoverZoomOff'); 
+    }
+    
+    // If titleBarTop is set to 'on' then move it up
+    if (set.titleBarTop === "on") {
+        $slider.addClass('titleBarTop'); 
+    }
+
+    // function to start auto play
+    function startSlider() {
+      sliderIntervalID = setInterval(function() {
+        nextSlide();
+      }, set.slidePause);
+    }
+    
+    // on mouseover stop the autoplay
+    $slider.mouseover(function() {
+      if (set.autoPlay === "on") {
+        clearInterval(sliderIntervalID);
+      }
+    });
+      
+    // on mouseout starts the autoplay
+    $slider.mouseout(function() {
+      if (set.autoPlay === "on") {
+        startSlider();
+      }
+    });
+
+    //on right arrow click
+    $slider.find("> .right").click(nextSlide)
+
+    //on left arrow click
+    $slider.find("> .left").click(prevSlide);
+      
+    // Go to next slide
+    function nextSlide() {
+      position = $slider.find(".show").index() + 1;
+      if (position > size - 1) position = 0;
+      changeCarousel(position);
+    }
+    
+    // Go to previous slide
+    function prevSlide() {
+      position = $slider.find(".show").index() - 1;
+      if (position < 0) position = size - 1;
+      changeCarousel(position);
+    }
+
+    //when user clicks slider button
+    $slider.find(" > ul > li").click(function() {
+      position = $(this).index();
+      changeCarousel($(this).index());
+    });
+
+    //this changes the image and button selection
+    function changeCarousel() {
+      $slider.find(".show").removeClass("show").fadeOut();
+      $slider
+        .find("> div")
+        .eq(position)
+        .fadeIn(set.fadeSpeed)
+        .addClass("show");
+      // The Dots
+      $slider.find("> ul").find(".showli").removeClass("showli");
+      $slider.find("> ul > li").eq(position).addClass("showli");
+    }
+
+    return $slider;
+  };
+})(jQuery);
+
+
+ 
+//////////////////////////////////////////////
+// Activate each slider - change options
+//////////////////////////////////////////////
+$(document).ready(function() {
+  
+  $("#slider1").sliderResponsive({
+  // Using default everything
+    // slidePause: 5000,
+    // fadeSpeed: 800,
+    // autoPlay: "on",
+    // showArrows: "off", 
+    // hideDots: "off", 
+    // hoverZoom: "on", 
+    // titleBarTop: "off"
+  });
+  
+  $("#slider2").sliderResponsive({
+    fadeSpeed: 300,
+    autoPlay: "off",
+    showArrows: "on",
+    hideDots: "on"
+  });
+  
+  $("#slider3").sliderResponsive({
+    hoverZoom: "off",
+    hideDots: "on"
+  });
+  
+}); 
+
 
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -87,9 +87,8 @@ class Home extends Component {
 
   sellers = () => {
     return (
-      <section class="best-seel-area pt--80 pb--60">
-        <div class="container">
-          <div class="row">
+      <div class="container-fluid publisher-slider pb-5">
+       <div class="row">
             <div class="col-lg-12">
               <div class="section__title text-center pb--50">
                 <h2 class="title__be--2">
@@ -103,34 +102,44 @@ class Home extends Component {
               </div>
             </div>
           </div>
-        </div>
-
-        <div>
-          <div
-            class="d-flex justify-content-between pl-2 pr-2"
-            style={{ zIndex: 1, position: "relative", top: 115 }}
-          >
-            <div class="publisher-icon pLeft d-flex justify-content-center">
-              <i class="zmdi zmdi-chevron-left pubIcon" />
+    <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
+        <div class="carousel-inner carousel-innerx row w-100 mx-auto" role="listbox">
+            <div class="carousel-item carousel-itemx col-md-3  active">
+               <div class="panel panel-default">
+                  <div class="panel-thumbnail">
+                    <a href="#" title="image 1" class="thumb">
+                      <img class="img-fluid mx-auto d-block" src="images/publishers/3.jpg" alt="slide 1" style={{height: 200, width: 'auto'}}/>
+                    </a>
+                  </div>
+                </div>
             </div>
-            <div class="publisher-icon p d-flex justify-content-center">
-              <i class="zmdi zmdi-chevron-right pubIcon" />
-            </div>
-          </div>
-          <div
-            class="containerx itemx-wrapper"
-            style={{ zIndex: -1, marginLeft: 70 }}
-          >
             {publishers.map((item, index) => {
               return (
-                <div class="itemx">
-                  <img src={item.image} alt="" />
+                <div class="carousel-item carousel-itemx col-md-3  ">
+                <div class="panel panel-default">
+                  <div class="panel-thumbnail">
+                    <a href="#" title="image 2" class="thumb">
+                     <img class="img-fluid mx-auto d-block" src={item.image} alt="slide 8" style={{height: 200, width: 'auto'}}/>
+                    </a>
+                  </div>
+                  
                 </div>
+            </div>
               );
             })}
-          </div>
+             
         </div>
-      </section>
+        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
     );
   };
 
