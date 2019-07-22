5cce3abce47f2554c37dc86afe6b7cf2f2f2a45f
Sayaf Ismael <sayaf0007@gmail.com>
2019-07-22 15:18:53 +0600

    working on new design

--- src/components/navbarX.jsx
+++ src/components/navbarX.jsx
@@ -10,7 +10,6 @@ import {
   // Prompt
 } from "react-router-dom";
 import ReactTyped from "react-typed";
-import FontAwesome from "react-fontawesome";
 
 class navbar extends Component {
   constructor(props) {
@@ -73,11 +72,10 @@ class navbar extends Component {
 
   render() {
     return (
-      <header id="wn__header" class="header__area ">
-        <div style={{backgroundColor: '#C67935'}}>
+      <header id="wn__header" class="header__area sticky__header">
         <div class="container-fluid">
           <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-6 col-sm-6 col-6 col-lg-2">
               <div class="logo">
                 <a href="/">
                   <img
@@ -89,11 +87,93 @@ class navbar extends Component {
                 </a>
               </div>
             </div>
-           
-            <div class="col-md-5">
-              <ul class="header__sidebar__right d-flex align-items-center">
-                <form class="form-inline searchAll"  style={{ width: "100%" }}>
-                  <div class="row m-0"  style={{ width: "100%", backgroundColor: 'white', borderRadius: 26}}>
+            <div class="col-md-5 d-none d-lg-block">
+              <nav class="mainmenu__nav">
+                <ul class="meninmenu d-flex justify-content-center">
+                  {/* <li class="drop with--one--item ">
+                    <a href="index.html">Home</a>
+                  </li> */}
+
+                  <li class="drop">
+                    <a>ক্যাটাগরী</a>
+                    <div class="megamenu mega03">
+                      <ul class="item item03">
+                        <li class="title">নতুন বই</li>
+                        <li>
+                          <Link to={"/shopGrid"}>মুক্তিযুদ্ধ </Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>বিজ্ঞান </Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>পশ্চিম বঙ্গের বই </Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>শিশুতোষ </Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>কিশোর সাহিত্য </Link>
+                        </li>
+                      </ul>
+                      <ul class="item item03">
+                        <li>বেস্ট সেলার বই</li>
+                        <li>
+                          <Link to={"/shopGrid"}>ধর্মিয় বই</Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>নিষিদ্ধ বই</Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>পাঠক কর্নার</Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>ব্লগ</Link>
+                        </li>
+                      </ul>
+                      {/* <ul class="item item03">
+                        <li>Collections</li>
+                        <li>
+                          <Link to={"/shopGrid"}>Science </Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>Fiction/Fantasy</Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>Self-Improvemen</Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>Home & Garden</Link>
+                        </li>
+                        <li>
+                          <Link to={"/shopGrid"}>Humor Books</Link>
+                        </li>
+                      </ul> */}
+                    </div>
+                  </li>
+
+                  <li class="drop">
+                    <a href="#">প্রকাশনী</a>
+                    <div class="megamenu dropdown">
+                      <ul class="item item01">
+                        <li>
+                          <a href="#">বাতিঘর প্রকাশনী</a>
+                        </li>
+                        <li>
+                          <a href="#">জ্ঞান আলো প্রকাশনী</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <a href="#">লেখক</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div class="col-md-6 col-sm-6 col-6 col-lg-5">
+              <ul class="header__sidebar__right d-flex justify-content-end align-items-center">
+                <form class="form-inline searchAll">
+                  <div class="row" style={{ width: 400 }}>
                     <ReactTyped
                       // typedRef={typedRef()}
                       loop={true}
@@ -102,7 +182,7 @@ class navbar extends Component {
                       startDelay={0}
                       backSpeed={20}
                       backDelay={2}
-                      strings={["পছন্দের বই টাইপ করুন সময়ের ব্যবচ্ছেদ", ]}
+                      strings={["পছন্দের বই খুজতে এখানে টাইপ করুন", "Search by books (ex. সময়ের ব্যাবচ্ছেদ)", "Search by Authors (ex. জাফর ইকবাল)", "Search by Publishers (ex. বাতিঘর)"]}
                       stopped={null}
                       smartBackspace
                       shuffle={false}
@@ -113,19 +193,16 @@ class navbar extends Component {
                     >
                       <input
                         type="search"
-                        style={{ width: "100%", border: 'none', height: 53}}
+                        style={{ width: 300 }}
                         class="form-control mr-sm-2"
                       />
                     </ReactTyped>
-                    <button class="btn my-2 my-sm-0 search-btn" type="submit" >
-                    <FontAwesome name='search' style={{color: 'white'}}/>
+                    <button class="btn my-2 my-sm-0" type="submit">
+                      Search
                     </button>
                   </div>
                 </form>
-              </ul>
-            </div>
-            <div class="col-md-4">
-              <ul class="header__sidebar__right d-flex justify-content-center align-items-center">
+
                 <li class="wishlist">
                   <a href="#" />
                 </li>
@@ -321,9 +398,86 @@ class navbar extends Component {
             </div>
           </div>
 
+          <div class="row d-none">
+            <div class="col-lg-12 d-none">
+              <nav class="mobilemenu__nav">
+                <ul class="meninmenu">
+                  <li>
+                    <a href="index.html">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">Pages</a>
+                    <ul>
+                      <li>
+                        <a href="about.html">About Page</a>
+                      </li>
+                      <li>
+                        <a href="portfolio.html">Portfolio</a>
+                        <ul>
+                          <li>
+                            <a href="portfolio.html">Portfolio</a>
+                          </li>
+                          <li>
+                            <a href="portfolio-details.html">
+                              Portfolio Details
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="my-account.html">My Account</a>
+                      </li>
+                      <li>
+                        <a href="cart.html">Cart Page</a>
+                      </li>
+                      <li>
+                        <a href="checkout.html">Checkout Page</a>
+                      </li>
+                      <li>
+                        <a href="wishlist.html">Wishlist Page</a>
+                      </li>
+                      <li>
+                        <a href="error404.html">404 Page</a>
+                      </li>
+                      <li>
+                        <a href="faq.html">Faq Page</a>
+                      </li>
+                      <li>
+                        <a href="team.html">Team Page</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a to={"/shopGrid"}>Shop</a>
+                    <ul>
+                      <li>
+                        <a to={"/shopGrid"}>Shop Grid</a>
+                      </li>
+                      <li>
+                        <a href="single-product.html">Single Product</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="blog.html">Blog</a>
+                    <ul>
+                      <li>
+                        <a href="blog.html">Blog Page</a>
+                      </li>
+                      <li>
+                        <a href="blog-details.html">Blog Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="contact.html">Contact</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
 
           <div class="mobile-menu d-block d-lg-none" />
-        </div>
         </div>
       </header>
     );
